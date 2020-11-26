eca1ef9ea7fa256eb17132891b84bf5a590c5935
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-11-26 20:13:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,5 @@ const mapToSquare = function(sourceArray) {
 
 // Reduce-like methods
 const reduceToTotal = function(sourceArray, startingPoint = 0) {
-  const reducer = function(accumulator, currentValue) { accumulator + currentValue };
-  return sourceArray.reduce(reducer, startingPoint);
+  return sourceArray.reduce(startingPoint);
 };
